85a5fdbea5f3efae4f219470fde8dd14926cf30e
adam.elkadaoui <adam.elkadaoui@gmail.com>
2024-11-27 19:21:00 +0100

    refactor: rename index.js to app.js

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const errorHandler = require('./middlewares/errorHandler.js');
-const sequelize = require('./config/database.js');
-const restaurantRoutes = require('./routes/restaurantRoutes.js');
-
-
+const errorHandler = require('./middlewares/errorHandler');
+const sequelize = require('./config/database');
+const restaurantRoutes = require('./routes/restaurantRoutes');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(errorHandler);
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/restaurants', restaurantRoutes);
+
 app.all('*', (req, res, next) => {
   next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
 });
 
-
-app.use('/api/restaurants', restaurantRoutes);
+app.use(errorHandler);
 
 sequelize.sync({ force: false })
   .then(() => console.log('Database synced'))
   .catch(err => console.error('Error syncing database:', err));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = server;
